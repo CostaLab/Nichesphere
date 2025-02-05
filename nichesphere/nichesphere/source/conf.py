140d7390911962d4f013abb128f6345f55f2dfab
MayraRuizT <aryam950812@gmail.com>
2025-02-05 14:29:26 +0100

    another path

--- nichesphere/nichesphere/source/conf.py
+++ nichesphere/nichesphere/source/conf.py
@@ -11,6 +11,7 @@ sys.path.insert(0, os.path.abspath('../../../../'))
 sys.path.insert(0, os.path.abspath('../../../'))
 sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
