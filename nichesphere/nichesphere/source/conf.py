b1148ee9b255567f91f680c769ca64dae3eb79b8
MayraRuizT <aryam950812@gmail.com>
2025-02-05 15:17:09 +0100

    no autoapi

--- nichesphere/nichesphere/source/conf.py
+++ nichesphere/nichesphere/source/conf.py
@@ -24,10 +24,7 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_rtd_theme', 'nbsphinx', 'sphinx.ext.autosummary', 'autoapi.extension']
-
-autoapi_dirs = ['../']
-autoapi_modules = {'tl': None}
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_rtd_theme', 'nbsphinx', 'sphinx.ext.autosummary']
 
 templates_path = ['_templates']
 exclude_patterns = []
